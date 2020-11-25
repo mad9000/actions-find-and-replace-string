9a864c7ba7a47418ab79d69c14e91e0d30bd9a11
Kishyr Ramdial <kishyr@mad.app>
2020-11-25 13:27:03 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+    const find = core.getInput('find')
+    const replace = core.getInput('replace')
+    const branchName = github.context.branchName.replace(find, replace)
+
+    core.setOutput('value', branchName)
+    console.log('branch name:', branchName)
+} catch (e) {
+    core.setFailed(error.message)
+}
