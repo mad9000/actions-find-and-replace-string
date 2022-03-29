14f2f3611e6ef0265d3199830234c4099d68fa19
André Cabrera <andre@andrecabrera.com>
2022-03-29 01:10:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ try {
     const replace = core.getInput('replace')
     const replaceAllInput = core.getInput('replaceAll')
     const replaceAll = replaceAllInput ? replaceAllInput == 'true' : false
-    const branchName = source.replaceAll(find, replace)
+    const branchName = replaceAll ? source.replaceAll(find, replace) : source.replace(find, replace)
     core.setOutput('value', branchName)
 } catch (error) {
     core.setFailed(error.message)
