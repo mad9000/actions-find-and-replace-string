0dc48db62e97d783863e113caab3ccbc3ad4320d
André Cabrera <andre@andrecabrera.com>
2022-03-29 01:09:45 -0300

    feat: replace all option to replace all occurences
    adds a replaceAll: (true/false) option to replace all occurences

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ try {
     const source = core.getInput('source')
     const find = core.getInput('find')
     const replace = core.getInput('replace')
-    const branchName = source.replace(find, replace)
+    const replaceAllInput = core.getInput('replaceAll')
+    const replaceAll = replaceAllInput ? replaceAllInput == 'true' : false
+    const branchName = source.replaceAll(find, replace)
     core.setOutput('value', branchName)
 } catch (error) {
     core.setFailed(error.message)
